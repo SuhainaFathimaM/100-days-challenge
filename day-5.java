931068791915139a0aa8fc09f2cbc8b938721e19
Suhaina Fathima M <153417476+SuhainaFathimaM@users.noreply.github.com>
2024-08-12 08:14:48 +0530

    Update day-5.java

--- day-5.java
+++ day-5.java
@@ -1,1 +1,20 @@
+import java.util.*;
+
+ 
+
+ class Controlstructure 
+ {
+   public static void main(String args[])
+   { 
+     String grade = "A";
+     if (grade == "O")
+       System.out.println("Grade points: 10");
+     else if (grade == "A")
+       System.out.println("Grade points: 9");
+     else if (grade == "B")
+       System.out.println("Grade points: 8");
+     else
+       System.out.println("Grade points is less than 8");
+   }
+ }
 
