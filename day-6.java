96bdde903ca4090ae809247ea97c3bb3fc9bf047
Suhaina Fathima M <153417476+SuhainaFathimaM@users.noreply.github.com>
2024-08-13 08:30:35 +0530

    Create day-6.java

---
+++ day-6.java
@@ -0,0 +1,13 @@
+public class StringCaseConversion {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        
+        
+        String upperCaseStr = str.toUpperCase();
+        System.out.println("Upper Case: " + upperCaseStr);
+        
+        
+        String lowerCaseStr = str.toLowerCase();
+        System.out.println("Lower Case: " + lowerCaseStr);
+    }
+}
