eb2b3cbe0aaabc48e78b8c9498d9bb16aa0f966a
Suhaina Fathima M <153417476+SuhainaFathimaM@users.noreply.github.com>
2024-08-15 22:47:25 +0530

    Create day-8.java

---
+++ day-8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+
+        System.out.print("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+
+        double area = length * width;
+
+        System.out.println("The area of the rectangle is: " + area);
+
+        scanner.close();
+    }
+}
