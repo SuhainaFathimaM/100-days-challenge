f54267eedc7f68098cf84cac8f2abcf318b45e25
Suhaina Fathima M <153417476+SuhainaFathimaM@users.noreply.github.com>
2024-08-23 19:14:51 +0530

    Update

---
+++ day-16.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class pattern {
+    public static void printPattern(int n)
+    {
+        int i, j;
+        
+        for (i = 0; i < n; i++) {
+            
+            for (j = 0; j < n; j++) {
+
+                if (i == 0 || j == 0 || i == n - 1
+                    || j == n - 1) {
+                    System.out.print("*");
+                }
+               
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
